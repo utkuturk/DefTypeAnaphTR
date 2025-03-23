b0af4526be7c083af4e8740907bc785c05a1c259
Utku Turk <utkuturk@umd.edu>
2025-03-23 12:52:46 -0400

    exp2

---
+++ exp1/read_pcibex.R
@@ -0,0 +1,35 @@
+read.pcibex <- function(filepath, auto.colnames = TRUE, fun.col = function(col, cols) {
+  cols[cols == col] <- paste(col, "Ibex", sep = ".")
+  return(cols)
+}) {
+  n.cols <- max(count.fields(filepath, sep = ",", quote = NULL), na.rm = TRUE)
+  if (auto.colnames) {
+    cols <- c()
+    con <- file(filepath, "r")
+    while (TRUE) {
+      line <- readLines(con, n = 1, warn = FALSE)
+      if (length(line) == 0) {
+        break
+      }
+      m <- regmatches(line, regexec("^# (\\d+)\\. (.+)\\.$", line))[[1]]
+      if (length(m) == 3) {
+        index <- as.numeric(m[2])
+        value <- m[3]
+        if (index < length(cols)) {
+          cols <- c()
+        }
+        if (is.function(fun.col)) {
+          cols <- fun.col(value, cols)
+        }
+        cols[index] <- value
+        if (index == n.cols) {
+          break
+        }
+      }
+    }
+    close(con)
+    return(read.csv(filepath, comment.char = "#", header = FALSE, col.names = cols))
+  } else {
+    return(read.csv(filepath, comment.char = "#", header = FALSE, col.names = seq(1:n.cols)))
+  }
+}
